391badc77b168b684682690cd789202fa76545fe
ankit <ankitxrajpoot@gmail.com>
2024-07-09 19:10:18 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cron = require('node-cron');
 
 const {PORT} = require('./config/serverConfig');
-const {sendBasicEmail} = require('./services/email-service');
+// const {sendBasicEmail} = require('./services/email-service');
 
 const setupAndStartServer = () =>{
     const app = express();
@@ -13,12 +14,17 @@ const setupAndStartServer = () =>{
         console.log(`Server Started on port no:  ${PORT}`);
 
 
-        sendBasicEmail(
-            '"Support" <support@admin.com>',
-            'ankitxrajpoot@gmail.com',
-            'This is a testing email',
-            'Hey! how are you, I hope you like the support'
-        )
+        // sendBasicEmail(
+        //     '"Support" <support@admin.com>',
+        //     'ankitxrajpoot@gmail.com',
+        //     'This is a testing email',
+        //     'Hey! how are you, I hope you like the support'
+        // )
+
+
+        cron.schedule('*/2 * * * *', ()=>{
+            console.log('running a task evry two minutes');
+        });
     });
 } 
 
