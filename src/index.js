b0dfd2744fb9dc7d98f8200c73c902bc8956905f
ankit <ankitxrajpoot@gmail.com>
2024-07-10 12:01:29 +0530

    Added notification crons

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cron = require('node-cron');
 
 const {PORT} = require('./config/serverConfig');
 // const {sendBasicEmail} = require('./services/email-service');
+const jobs = require('./utils/job');
+const TicketController = require('./controllers/ticket-controller');
 
 const setupAndStartServer = () =>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    app.post('/api/v1/tickets', TicketController.create);
 
     app.listen(PORT, () =>{
         console.log(`Server Started on port no:  ${PORT}`);
@@ -22,9 +25,7 @@ const setupAndStartServer = () =>{
         // )
 
 
-        cron.schedule('*/2 * * * *', ()=>{
-            console.log('running a task evry two minutes');
-        });
+        jobs();
     });
 } 
 
